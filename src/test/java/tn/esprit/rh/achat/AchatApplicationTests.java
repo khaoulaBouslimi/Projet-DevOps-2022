c6ab6a9348a3660ec9c6a0e666a1fc8f5158a853
Aziz Ben Kbaier <73747555+aziz223@users.noreply.github.com>
2022-11-11 01:57:36 +0100

    Update AchatApplicationTests.java

--- src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
+++ src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
@@ -1,4 +1,4 @@
-package tn.esprit.rh.achat;
+/*package tn.esprit.rh.achat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class AchatApplicationTests {
     @Test
     void contextLoads() {
     }
-}
+}*/
